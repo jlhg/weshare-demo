5dc5dea087c53c60287e1f0a7f90e15ae4ce013a
Jian-Long Huang <huang@jianlong.org>
2017-07-23 07:41:29 +0000

    Change the usage and config of predictor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,14 @@ class HomeController < ApplicationController
 
   def upload
     uploaded_file = params[:uploaded_file]
-    result = `python #{Settings.predictor_path} #{uploaded_file.path}`
+    result = `curl #{Settings.predictor_url}?input=#{uploaded_file.path}`
     case result
     when /result :1/
       render json: { result: "您的車已在停車格內，請繼續下一步。" }
     when /result :0/
       render json: { result: "很抱歉，您的車不在停車格內，請停妥後再次嘗試。" }
     else
+      Rails.logger.debug result
       render json: { result: "影像辨識失敗。" }
     end
   end
